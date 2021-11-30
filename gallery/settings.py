94789d340e46fb8ecf99fba71fb93194b3d0e506
Shawn9717 <shawn.njonga@student.moringaschool.com>
2021-11-30 03:03:56 -0500

    add database

--- gallery/settings.py
+++ gallery/settings.py
@@ -116,11 +116,11 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
- DATABASES = {
+DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.postgresql',
-         'NAME':'gallary',
-        'USER':'shawn',
+         'NAME':'gallery',
+         'USER':'shawn',
      'PASSWORD':'qwanza',
      }
  }
@@ -129,20 +129,20 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+#AUTH_PASSWORD_VALIDATORS = [
+#    {
+#        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#   },
+#   {
+#        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#   },
+#    {
+#       'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+#    },
+#   {
+#        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+#   },
+#]
 
 
 # Internationalization

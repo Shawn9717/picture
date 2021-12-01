c94b073f53bdf7c1b554cacf402042fbc9db78b8
Shawn9717 <shawn.njonga@student.moringaschool.com>
2021-12-01 05:30:55 -0500

    deplpoyment

--- photos/admin.py
+++ photos/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Location,Category,Image
 
-# Register your models here.
+# Register your models
 admin.site.register(Image)
 admin.site.register(Location)
 admin.site.register(Category)

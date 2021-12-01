f0d9c2c41d9fef737a37c7310ded3cb299380b8b
Shawn9717 <shawn.njonga@student.moringaschool.com>
2021-12-01 04:39:22 -0500

    add cloudinary secreat key and api

--- gallery/settings.py
+++ gallery/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'cloudinary,',
+    'cloudinary',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
